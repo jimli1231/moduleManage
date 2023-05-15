803c96ec6e6bd0d029c4eb6445e9e49798dab327
jimli1231 <7422555+tryxiaoli@user.noreply.gitee.com>
2023-05-15 23:06:19 +0800

    feat:旋转

--- js/dragBehavior.js
+++ js/dragBehavior.js
@@ -1,12 +1,39 @@
-   //拖动方法
-   export const createDragBehavior = (model) => {
-    const pointerDragBehavior = new BABYLON.PointerDragBehavior({
-      dragPlaneNormal: new BABYLON.Vector3(0, 1, 0),
-    });
-    pointerDragBehavior.useObjectOrienationForDragging = false;
-    pointerDragBehavior.moveAttached = true;
-    pointerDragBehavior.onDragObservable.add((event) => {
-      model.position.addInPlace(event.delta);
-    });
-    model.addBehavior(pointerDragBehavior);
-  };
+//拖动方法
+ const createMoveBehavior = (model) => {
+  const pointerDragBehavior = new BABYLON.PointerDragBehavior({
+    dragPlaneNormal: new BABYLON.Vector3(0, 1, 0),
+  });
+  pointerDragBehavior.useObjectOrienationForDragging = false;
+  pointerDragBehavior.moveAttached = true;
+  pointerDragBehavior.onDragObservable.add((event) => {
+    model.position.addInPlace(event.delta);
+  });
+  model.addBehavior(pointerDragBehavior);
+};
+
+//旋转方法
+ const createRotateBehavior = (model) => {
+  const pointerDragBehavior = new BABYLON.PointerDragBehavior({
+    dragPlaneNormal: new BABYLON.Vector3(0, 1, 0),
+  });
+  let angel = 0;
+  pointerDragBehavior.useObjectOrienationForDragging = false;
+  pointerDragBehavior.moveAttached = false; // 修改为 false，禁用自动移动
+  pointerDragBehavior.onDragObservable.add((event) => {
+    // 计算鼠标拖拽距离的旋转角度（以弧度为单位）
+    console.log();
+    const rotationAngle = event.delta.x * 10; // 你可以根据需要调整除以的数值，以改变旋转速度
+    angel = angel + rotationAngle;
+    // 创建一个以模型为中心的旋转矩阵
+    model.rotation = new BABYLON.Vector3(0, angel, 0);
+  });
+  model.addBehavior(pointerDragBehavior);
+};
+export const createDropBehavior = (model, type) => {
+  debugger
+  if (type == createMoveBehavior) {
+    createMoveBehavior(model);
+  } else if (type == createRotateBehavior) {
+    createRotateBehavior(model);
+  }
+};
--- js/init.js
+++ js/init.js
@@ -1,13 +1,18 @@
-import { createScene } from './scene.js';
-import { createDragBehavior } from './dragBehavior.js';
-import { showDialog } from './dialog.js';
-import { handlerProxy } from './common/utils.js';
-import { deleteButtonInit, exportButtonInit } from './btns.js';
+import { createScene } from "./scene.js";
+import { createDropBehavior } from "./dragBehavior.js";
+import { showDialog } from "./dialog.js";
+import { handlerProxy } from "./common/utils.js";
+import { deleteButtonInit, exportButtonInit } from "./btns.js";
 const proxySelectedMesh = new Proxy({ value: null }, handlerProxy);
+const proxyCurrentOrders = new Proxy(
+  { value: "createMoveBehavior" },
+  handlerProxy
+);
+
 export const init = async (engine) => {
   const scene = await createScene(engine);
-  let deleteButton = deleteButtonInit(scene, proxySelectedMesh)
-  let exportButton = exportButtonInit(scene, proxySelectedMesh)
+  let deleteButton = deleteButtonInit(scene, proxySelectedMesh);
+  let exportButton = exportButtonInit(scene, proxySelectedMesh);
   engine.runRenderLoop(() => {
     scene.render();
   });
@@ -40,18 +45,19 @@ export const init = async (engine) => {
           scene
         );
         model.meshes.forEach((mesh) => {
-
-       
-          if (mesh.name !== "factoryFloor" && mesh.name !== "__root__") {   // 添加此行以跳过地板
+          if (mesh.name !== "factoryFloor" && mesh.name !== "__root__") {
+            // 添加此行以跳过地板
             if (!mesh.ownId) {
-              mesh.ownId = id
-              mesh.ownScale = scale
-              mesh.ownPath = modelPath
+              mesh.ownId = id;
+              mesh.ownScale = scale;
+              mesh.ownPath = modelPath;
             }
-            mesh.scaling = new BABYLON.Vector3(  mesh.ownScale,   mesh.ownScale,   mesh.ownScale);
-
-
-            createDragBehavior(mesh); // Apply drag behavior to each mesh
+            mesh.scaling = new BABYLON.Vector3(
+              mesh.ownScale,
+              mesh.ownScale,
+              mesh.ownScale
+            );
+            createDropBehavior(mesh, proxyCurrentOrders.value);
             mesh.actionManager = new BABYLON.ActionManager(scene);
             mesh.actionManager.registerAction(
               new BABYLON.ExecuteCodeAction(
